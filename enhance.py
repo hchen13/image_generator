8380c1b44256dc1a66831678415591bc0948b4a6
Ethan Chen <gradschool.hchen13@gmail.com>
2020-09-02 16:25:25 +0800

    add enlarge and noise cancel steps

--- enhance.py
+++ enhance.py
@@ -1,6 +1,8 @@
+from datetime import datetime
+
 import cv2
 import numpy as np
-from ISR.models import RRDN
+from ISR.models import RRDN, RDN
 from PIL import Image
 
 
@@ -36,12 +38,25 @@ def show_image(*images, width=None, col=None, wait=0, title=None, destroy=False)
 if __name__ == '__main__':
     image_path = 'exports/c497/0.jpg'
     origin = cv2.imread(image_path)
-    print('[info] initializing model...')
-    rdn = RRDN(weights='gans')
-    print("[info] complete.\n")
 
-    print('[info] predicting...')
-    output = rdn.predict(origin)
-    print("[info] done.\n")
-    print(output.shape)
-    show_image(origin, output, width=1000)
+    print('[info] initializing model...')
+    tick = datetime.now()
+    m1 = RRDN(weights='gans')
+    m2 = RDN(weights='noise-cancel')
+    tock = datetime.now()
+    print(f"[info] complete, time elapsed: {(tock - tick).total_seconds():.1f}s.\n")
+
+    print('[info] enlarging...')
+    tick = datetime.now()
+    enlarged = m1.predict(origin)
+    tock = datetime.now()
+    print(f"[info] done, time elapsed: {(tock - tick).total_seconds():.2f}s")
+
+    print('[info] cancelling noise...')
+    tick = datetime.now()
+    cancelled = m2.predict(enlarged, by_patch_of_size=50)
+    tock = datetime.now()
+    print(f"[info] done, time elapsed: {(tock - tick).total_seconds():.2f}s\n")
+
+    print(f"original image size: {origin.shape}, enlarged size: {enlarged.shape}, final size: {cancelled.shape}")
+    show_image(origin, enlarged, cancelled, width=1000)
